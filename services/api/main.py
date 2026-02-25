b08e04b856e8cfccc227ff89652234c8c330d98f
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-02-25 20:50:28 +0100

    feat(api): optional Bearer token auth

--- services/api/main.py
+++ services/api/main.py
@@ -1,12 +1,39 @@
 from typing import Any
 
 from fastapi import FastAPI, WebSocket
+from fastapi.openapi.utils import get_openapi
 
 from api.config import settings
 from api.routers.auth import router as auth_router
 from api.websockets import WebSocketsClient
 
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="Duo API",
+        version="1.0",
+        routes=app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+
+    openapi_schema["security"] = [{"BearerAuth": []}]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
 app = FastAPI()
+app.openapi = custom_openapi
 app.include_router(auth_router, prefix='/auth', tags=['auth'])
 
 

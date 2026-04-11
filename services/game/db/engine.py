117f19b0dfd30e91ce2e288f7fa0effa7a6ed67c
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-04-11 08:38:27 +0200

    feat(game): database engine and session manager

---
+++ services/game/db/engine.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from services.game.config import settings
+
+
+def get_db_engine() -> AsyncEngine:
+    return create_async_engine(str(settings.db_dsn))
+
+
+def get_async_session() -> AsyncSession:
+    return AsyncSession(get_db_engine())
+
+
+@asynccontextmanager
+async def get_session_ctx(session: AsyncSession | None = None):
+    if session is not None:
+        yield session
+    else:
+        async with get_async_session() as s:
+            yield s

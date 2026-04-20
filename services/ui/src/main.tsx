2e1854a7ec8bbd745dfc7dcae252a200774d21c8
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-04-20 21:27:28 +0200

    feat(components): install layout component

--- services/ui/src/main.tsx
+++ services/ui/src/main.tsx
@@ -4,8 +4,8 @@ import { createRoot } from "react-dom/client"
 
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import router from "@/router"
-import { RouterProvider } from "react-router"
 import "@/styles/index.css"
+import { RouterProvider } from "react-router"
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>

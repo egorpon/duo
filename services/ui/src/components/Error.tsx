2dd345f859eaac864c9ab736981e4fd4c00dd1c1
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-04-25 17:14:33 +0200

    feat(not-found): return home button

--- services/ui/src/components/Error.tsx
+++ services/ui/src/components/Error.tsx
@@ -1,11 +1,19 @@
-import { useRouteError, isRouteErrorResponse } from "react-router"
+import { useRouteError, isRouteErrorResponse, Link } from "react-router"
+import { Button } from "./ui/button"
 
 export function Error() {
     const error = useRouteError()
     if (!isRouteErrorResponse(error)) {
         return <div>Some error happend</div>
     }
-    return <div>I know what happend: {error.statusText}</div>
+    return (
+        <div>
+            <div>I know what happend: {error.statusText}</div>
+            <Button>
+                <Link to="/">Back to home</Link>
+            </Button>
+        </div>
+    )
 }
 
 export default Error

ae8924cdf556f17da3521a82d9f716cb203bceb7
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-05-03 12:07:37 +0200

    feat(websockets): send messages two-way, ws manager

--- services/api/routers/websockets.py
+++ services/api/routers/websockets.py
@@ -1,4 +1,6 @@
+import json
 import logging
+from typing import Any, Mapping
 
 from fastapi import APIRouter, WebSocket
 from fastapi.websockets import WebSocketDisconnect
@@ -8,23 +10,51 @@ logger = logging.getLogger('duo.api.websockets')
 router = APIRouter(tags=['websocket'])
 
 
+class WebSocketManager:
+    def __init__(self) -> None:
+        self.connections: list[WebSocket] = []
+
+    def add_connection(self, socket: WebSocket) -> None:
+        self.connections.append(socket)
+
+    def remove_connection(self, socket: WebSocket) -> None:
+        self.connections.remove(socket)
+
+    async def send_all(self, message: Mapping[str, Any]) -> None:
+        for connection in self.connections:
+            try:
+                await connection.send_json(message)
+            except Exception:
+                logger.exception('failed to send message to %s', connection)
+                pass
+
+
+manager = WebSocketManager()
+
+
 @router.websocket('/games/{game_id}/')
 async def play_game(websocket: WebSocket, game_id: int) -> None:
     logger.debug('game %s: got connection', game_id)
     try:
         await websocket.accept()
+        manager.add_connection(websocket)
         logger.debug('game %s: accepted ws connection', game_id)
+        await manager.send_all({'message': 'someone new'})
         while True:
             logger.debug('waiting for message')
             data = await websocket.receive()
             if data.get('type') == 'websocket.disconnect':
                 logger.debug('client disconnected')
                 return
+
             logger.debug('received message: %s', data)
-            logger.debug('sending reply')
-            await websocket.send_json({'text': 'hello'})
+            text: str = data.get('text', '')
+            await manager.send_all({'message': text})
 
     except WebSocketDisconnect:
         logger.exception('Client disconnected')
     except Exception:
         logger.exception('Unexpected exception')
+    finally:
+        manager.remove_connection(websocket)
+        await manager.send_all({'message': 'someone left'})
--- services/ui/src/App.tsx
+++ services/ui/src/App.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { Input } from "./components/ui/input"
 
 const url = "http://localhost:8000/ws/games/1/"
 
 export default function App() {
     const wsRef = useRef<WebSocket | null>(null)
+    const [messages, setMessages] = useState<string[]>([])
+    const [message, setMessage] = useState<string>("")
     useEffect(() => {
         const ws = new WebSocket(url)
-        wsRef.current = new WebSocket(url)
+        wsRef.current = ws
         if (wsRef === null) {
             return
         }
@@ -20,22 +23,34 @@ export default function App() {
             console.log("ws closed")
         })
         ws.addEventListener("message", (event: MessageEvent) => {
-            console.log("received:", event)
+            setMessages((prev) => [...prev, event.data])
         })
-        return () => {
-            ws.close()
-        }
+        return () => ws.close()
     }, [])
 
     const handleClick = () => {
         if (!wsRef.current) {
+            console.log("no current ws", wsRef.current)
             return
         }
-        wsRef.current.send("Hello")
+        wsRef.current.send(message)
+        setMessage('')
     }
     return (
         <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
             <Button onClick={handleClick}>Click</Button>
+            <Input
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <div>
+                {messages.map((row, idx) => (
+                    <div key={idx}>
+                        {idx}: {row}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }

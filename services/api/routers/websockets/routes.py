d81df61bccfeda52d1e552ac6798af03f26fb07f
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-05-06 17:08:16 +0200

    refactor(websockets): simplify connections datastructure

--- services/api/routers/websockets/routes.py
+++ services/api/routers/websockets/routes.py
@@ -1,29 +1,70 @@
+import logging
+from typing import Any, MutableMapping
+
 from fastapi import APIRouter, WebSocket
 from fastapi.websockets import WebSocketDisconnect
 
-from services.api.routers.websockets.manager import logger, manager
+from generated import game_pb2, game_pb2_grpc
 from services.api.routers.websockets.types import (
     AuthenticatedMessage,
     AuthenticatedMessageBody,
-    ConnectedMessage,
-    ConnectedMessageBody,
     DisconnectedMessage,
     DisconnectedMessageBody,
-    GameMoveMessage,
-    GameMoveMessageBody,
 )
 from services.api.token import get_user_from_token
 
 router = APIRouter(tags=['websocket'])
 
+_logger = logging.getLogger('duo.api.websockets')
 
-@router.websocket('/games/{game}/')
+
+class GameLoggingAdapter(logging.LoggerAdapter[logging.Logger]):
+    def process(
+        self, msg: Any, kwargs: MutableMapping[str, Any]
+    ) -> tuple[Any, MutableMapping[str, Any]]:
+        if self.extra is None:
+            return msg, kwargs
+
+        game = self.extra.get('game', '_')
+        user = self.extra.get('user', '_')
+        return f'({game} : {user}) {msg}', kwargs
+
+
+logger = GameLoggingAdapter(logger=_logger)
+
+connections: dict[int, WebSocket] = {}
+
+
+def get_opponent(game: game_pb2.Game, user: int) -> int:
+    if game.player1 == user:
+        return game.player2
+    if game.player2 == user:
+        return game.player1
+
+    raise Exception('Opponent not found')
+
+
+@router.websocket('/games/{game_id}/')
 async def play_game(
     websocket: WebSocket,
-    game: int,
+    game_id: int,
 ) -> None:
     user: int | None = None
-    logger.extra = {'game': game}
+    logger.extra = {'game': game_id}
+
+    game_service: game_pb2_grpc.GameServiceAsyncStub = (  # pyright: ignore
+        game_pb2_grpc.GameServiceStub(websocket.app.state.game_channel)
+    )
+    try:
+        game = await game_service.GetGameById(
+            game_pb2.GetGameByIdRequest(game_id=game_id)
+        )
+        logger.debug('game present')
+    except Exception:
+        logger.debug('game not found')
+        await websocket.close(reason='not found')
+        return
+
     try:
         await websocket.accept()
         logger.debug('connection accepted')
@@ -36,37 +77,26 @@ async def play_game(
 
         logger.extra['user'] = user
         logger.debug('user verified')
-        manager.add_connection(game=game, player=user, socket=websocket)
-        await manager.message_player(
-            game=game,
-            player=user,
-            message=AuthenticatedMessage(
+        if not game.player2 and user != game.player1:
+            join_result = await game_service.JoinGame(
+                game_pb2.JoinGameRequest(game_id=game_id, player_id=user)
+            )
+            game = join_result.game
+
+        elif game.player2 and user not in [game.player1, game.player2]:
+            logger.debug('player does not belong to a game')
+            await websocket.close()
+            return
+
+        connections[user] = websocket
+        await websocket.send_json(
+            data=AuthenticatedMessage(
                 body=AuthenticatedMessageBody(
                     success=True,
                     message='Successfully authenticated',
                 ),
-            ),
+            ).model_dump_json(),
         )
-        await manager.message_opponent(
-            game=game,
-            player=user,
-            message=ConnectedMessage(
-                body=ConnectedMessageBody(
-                    message='Opponent connected',
-                ),
-            ),
-        )
-        while True:
-            data = await websocket.receive_json()
-            logger.debug('received message: %s', data)
-            text: str = data.get('message', '')
-            await manager.message_opponent(
-                game=game,
-                player=user,
-                message=GameMoveMessage(
-                    body=GameMoveMessageBody(game_move={'message': text}),
-                ),
-            )
 
     except WebSocketDisconnect:
         logger.debug('user disconnected')
@@ -74,13 +104,13 @@ async def play_game(
         logger.exception('Unexpected exception')
     finally:
         if user:
-            await manager.message_opponent(
-                game=game,
-                player=user,
-                message=DisconnectedMessage(
-                    body=DisconnectedMessageBody(
-                        message='Opponent disconnected'
-                    ),
-                ),
-            )
-            manager.remove_connection(game=game, player=user)
+            opponent = get_opponent(game=game, user=user)
+            if opponent in connections.keys():
+                await connections[opponent].send_json(
+                    data=DisconnectedMessage(
+                        body=DisconnectedMessageBody(
+                            message='Opponent disconnected'
+                        ),
+                    ).model_dump_json(),
+                )
+                connections.pop(user, None)

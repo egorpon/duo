94af92806698f39f26f8faae273506c22ccaec27
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-05-18 21:58:46 +0200

    feat(logging): configure root logger, and other loggers for gunicorn
    bind to unix socket instead of tcp port for performance

--- services/api/gunicorn_conf.py
+++ services/api/gunicorn_conf.py
@@ -1,8 +1,9 @@
 import multiprocessing
+from pathlib import Path
 
 from common.logging.main import get_logging_config
 
-bind = '0.0.0.0:8000'
+bind = 'unix:/opt/duo/app.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
-logconfig_dict = get_logging_config()
+logconfig_dict = get_logging_config(Path('/app/common/logging/config.json'))

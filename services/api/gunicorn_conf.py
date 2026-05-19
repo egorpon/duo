1e04242e12e4519438cb7d8ef7b06cb5cb55a013
Vladyslav Voinov <voinovvladyslav@gmail.com>
2026-05-19 21:18:31 +0200

    refactor(config): more config options for production
    a lot of possible overrides throgh .env

--- services/api/gunicorn_conf.py
+++ services/api/gunicorn_conf.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from common.logging.main import get_logging_config
 
-bind = 'unix:/opt/duo/app.sock'
-workers = multiprocessing.cpu_count() * 2 + 1
+bind = '0.0.0.0:8000'
+workers = max(multiprocessing.cpu_count() * 2, 1)
 worker_class = 'uvicorn.workers.UvicornWorker'
 logconfig_dict = get_logging_config(
-    Path('/app/common/logging/config.prod.json')
+    Path(__file__).parent.parent.parent / 'common/logging/config.prod.json'
 )

22655e917eefe59cd5186c217d4818ee0cf5831c
egorpon <ponomarenkoegor36@gmail.com>
2026-05-20 09:08:01 +0000

    refactor: reorganize service tests file structure and add config for tests

---
+++ services/auth/tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        parts = item.path.parts
+
+        if 'unit' in parts:
+            item.add_marker(pytest.mark.unit)
+        elif 'integration' in parts:
+            item.add_marker(pytest.mark.integration)
+        elif 'e2e' in parts:
+            item.add_marker(pytest.mark.e2e)
